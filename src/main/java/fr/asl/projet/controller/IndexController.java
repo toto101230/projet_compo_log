7dd34a9715d99a4531ca7b90dcb5fc4977eaf23e
toto101230 <tpienne51@gmail.com>
2023-12-19 11:26:26 +0100

    test d'ajout de la securitée

--- src/main/java/fr/asl/projet/controller/IndexController.java
+++ src/main/java/fr/asl/projet/controller/IndexController.java
@@ -3,26 +3,40 @@ package fr.asl.projet.controller;
 import fr.asl.projet.model.Client;
 import fr.asl.projet.model.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class IndexController {
     @Autowired
     private ClientRepository clientRepository;
 
     @PostMapping("/add")
+    @ResponseBody
     public String add(@RequestParam String login, @RequestParam String password) {
         clientRepository.save(new Client(login, password, "nom", "adresse", "mail"));
-        return "Ajout d'un utilisateur";
+        return "index";
     }
 
     @GetMapping("/list")
+    @ResponseBody
     public Iterable<Client> all() {
         return clientRepository.findAll();
     }
 
     @GetMapping("/find/{id}")
+    @ResponseBody
     public Client find(@PathVariable Integer id) {
         return clientRepository.findById(id).get();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
